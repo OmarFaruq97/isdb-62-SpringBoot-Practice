8de3cf8ed9a8ad562c7b673953bb8af5d01d8549
Omar Faruq <omarfaruq9723@gmail.com>
2025-03-03 19:31:24 +0600

    initial

--- practice/src/main/java/org/isdb_62_spring/practice/controller/DummyController.java
+++ practice/src/main/java/org/isdb_62_spring/practice/controller/DummyController.java
@@ -1,8 +1,13 @@
 package org.isdb_62_spring.practice.controller;
 
+
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
 @RestController
 @CrossOrigin(origins = "*")
 public class DummyController {
