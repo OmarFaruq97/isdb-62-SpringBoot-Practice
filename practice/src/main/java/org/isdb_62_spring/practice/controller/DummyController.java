5c388c07c325bd1f18deb196c429684ffc3525bf
Omar Faruq <omarfaruq9723@gmail.com>
2025-03-03 23:45:57 +0600

    quizapp initial
    first app project

--- practice/src/main/java/org/isdb_62_spring/practice/controller/DummyController.java
+++ practice/src/main/java/org/isdb_62_spring/practice/controller/DummyController.java
@@ -1,12 +1,10 @@
 package org.isdb_62_spring.practice.controller;
 
-
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 @CrossOrigin(origins = "*")
