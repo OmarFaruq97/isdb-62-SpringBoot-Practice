fa065041f19fc7775809894be82a9d38bc9319b8
Omar Faruq <omarfaruq9723@gmail.com>
2025-04-11 01:43:33 +0600

    initials

---
+++ security/src/main/java/com/org/isdb62/SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.org.isdb62;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+		System.out.println("Spring Boot Security project program is running Omar Faruq");
+	}
+}
