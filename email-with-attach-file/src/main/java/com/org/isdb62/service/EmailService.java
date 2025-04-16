d6df0867e59615aafaefb414fa349726fa5da38a
Omar Faruq <omarfaruq9723@gmail.com>
2025-04-16 21:04:03 +0600

    email-with-attach file operation successfully complete and separate

--- email-with-attach-file/src/main/java/com/org/isdb62/service/EmailService.java
+++ email-with-attach-file/src/main/java/com/org/isdb62/service/EmailService.java
@@ -1,26 +1,27 @@
 package com.org.isdb62.service;
 
-import com.org.isdb62.utils.Email;
 import com.org.isdb62.utils.EmailServiceUtils;
-import jakarta.mail.Message;
-import jakarta.mail.MessagingException;
-import jakarta.mail.Session;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
-import jakarta.mail.internet.MimeMultipart;
-import jakarta.mail.internet.MimeMessage.RecipientType;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.Base64;
 import java.util.Properties;
 
-import static java.awt.SystemColor.text;
+import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMultipart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.api.client.util.Base64;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Message;
+
+import jakarta.mail.Message.RecipientType;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class EmailService {
@@ -29,11 +30,11 @@ public class EmailService {
     private EmailServiceUtils emailServiceUtil;
 
     public void sendEmail(String to, String subject, String body)
-        throws MessagingException, IOException, GeneralSecurityException{
-        //Get Gmail Service
-        Email service = emailServiceUtil.getEmailService();
+            throws MessagingException, IOException, GeneralSecurityException {
+        // Get Gmail Service
+        Gmail service = emailServiceUtil.getGmailService();
 
-        //Create Email Content
+        // Create Email Content
         Properties properties = new Properties();
         Session session = Session.getDefaultInstance(properties, null);
         MimeMessage email = new MimeMessage(session);
@@ -43,7 +44,7 @@ public class EmailService {
         email.setSubject(subject);
         email.setText(body);
 
-        //Encode and send the email
+        // Encode and send the email
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         email.writeTo(buffer);
         byte[] bytes = buffer.toByteArray();
@@ -51,28 +52,53 @@ public class EmailService {
 
         Message message = new Message();
         message.setRaw(encodedEmail);
-        
-        //send the message
+
+        // send the message
         service.users().messages().send("me", message).execute();
     }
-    
+
     // This is send Email with attach file
     public void sendEmailWithAttachment(String to, String subject, String body, MultipartFile file)
-    throws MessagingException, IOException, GeneralSecurityException {
+			throws MessagingException, IOException, GeneralSecurityException {
 
-        Email service = emailServiceUtil.getEmailService();
+        Gmail service = emailServiceUtil.getGmailService();
 
-        //Create the email content
+        // Create the email content
         Properties properties = new Properties();
-        Session session = Session.getDefaultInstance(properties,null);
-        
+        Session session = Session.getDefaultInstance(properties, null);
 
         MimeMessage email = new MimeMessage(session);
         email.setFrom(new InternetAddress("me"));
         email.addRecipient(RecipientType.TO, new InternetAddress(to));
         email.setSubject(subject);
 
-        //Create multipart message
+        // Create multipart message
         MimeMultipart multipart = new MimeMultipart();
+
+        // Body part
+		MimeBodyPart bodyPart = new MimeBodyPart();
+		bodyPart.setText(body);
+		multipart.addBodyPart(bodyPart);
+
+        // Attachment part
+		MimeBodyPart attachmentPart = new MimeBodyPart();
+		attachmentPart.setFileName(file.getOriginalFilename());
+		attachmentPart.setContent(file.getBytes(), file.getContentType());
+		multipart.addBodyPart(attachmentPart);
+
+        // set content to email
+        email.setContent(multipart);
+
+       // Convert to Gmail Message
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		email.writeTo(buffer);
+		byte[] rawMessage = buffer.toByteArray();
+		String encodedEmail = Base64.encodeBase64URLSafeString(rawMessage); 
+
+        Message message = new Message();
+		message.setRaw(encodedEmail);
+
+		service.users().messages().send("me", message).execute();
+
     }
 }
