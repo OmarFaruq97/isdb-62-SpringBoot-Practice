91a53df448e9df638e402460d35d305a5d2eec47
Omar Faruq <omarfaruq9723@gmail.com>
2025-04-19 02:05:25 +0600

    invoice initial

---
+++ invoice/src/main/java/com/org/isdb62/InvoiceApplication.java
@@ -0,0 +1,15 @@
+package com.org.isdb62;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(InvoiceApplication.class, args);
+		System.out.println("riha you project is strat so don't worry. you need a come back");
+	}
+
+}
