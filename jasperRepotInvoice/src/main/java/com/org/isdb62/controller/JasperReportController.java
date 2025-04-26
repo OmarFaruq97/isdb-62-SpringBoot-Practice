23f13e842a9c0f77f1b062e3a0a9131a1797c9a8
Omar Faruq <omarfaruq9723@gmail.com>
2025-04-26 22:30:36 +0600

    jasperReportInvoice complete

--- jasperRepotInvoice/src/main/java/com/org/isdb62/controller/JasperReportController.java
+++ jasperRepotInvoice/src/main/java/com/org/isdb62/controller/JasperReportController.java
@@ -1,4 +1,43 @@
 package com.org.isdb62.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/reports")
 public class JasperReportController {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/invoice")
+    public void generateInvoiceReport(HttpServletResponse response) throws Exception {
+        ClassPathResource reportResource = new ClassPathResource("reports/Invoice.jrxml");
+        InputStream reportStream = reportResource.getInputStream();
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(reportStream);
+
+        try (Connection conn = dataSource.getConnection()) {
+            Map<String, Object> parameters = new HashMap<>();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, conn);
+
+            response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+            response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=invoice.pdf");
+
+            JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
+        }
+    }
 }
--- jasperRepotInvoice/src/main/java/com/org/isdb62/service/JasperReportService.java
+++ jasperRepotInvoice/src/main/java/com/org/isdb62/service/JasperReportService.java
@@ -1,4 +1,34 @@
 package com.org.isdb62.service;
 
+import net.sf.jasperreports.engine.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class JasperReportService {
+
+    @Autowired
+    private DataSource dataSource;
+
+    public byte[] generateInvoiceReport() throws JRException, SQLException, IOException {
+        ClassPathResource resource = new ClassPathResource("/reports/Invoice.jrxml");
+        InputStream reportStream = resource.getInputStream();
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(reportStream);
+
+        try (Connection conn = dataSource.getConnection()){
+            Map<String, Object> parameters = new HashMap<>();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, conn);
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        }
+    }
 }
