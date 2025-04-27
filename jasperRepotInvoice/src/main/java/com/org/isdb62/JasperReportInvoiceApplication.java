07951471b30c5a6ea42cd532c82f81e0248ac197
Omar Faruq <omarfaruq9723@gmail.com>
2025-04-27 09:25:06 +0600

    rename

--- jasperRepotInvoice/src/main/java/com/org/isdb62/JasperReportInvoiceApplication.java
+++ jasperRepotInvoice/src/main/java/com/org/isdb62/JasperReportInvoiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JasperRepotInvoiceApplication {
+public class JasperReportInvoiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JasperRepotInvoiceApplication.class, args);
+		SpringApplication.run(JasperReportInvoiceApplication.class, args);
 		System.out.println("your project is running riha go ahead");
 	}
 
