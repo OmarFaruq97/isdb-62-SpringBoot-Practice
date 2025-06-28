b3fb36fe9e408de994245dc3452f9a5b06f8adc6
Omar Faruq <omarfaruq9723@gmail.com>
2025-06-28 20:00:24 +0600

    WIP: Changes before switching branch

--- employee/src/main/java/com/isdb62/employee/config/AppConfig.java
+++ employee/src/main/java/com/isdb62/employee/config/AppConfig.java
@@ -12,28 +12,30 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 @Configuration
 public class AppConfig {
-	@Bean
-	public DataSource dataSource() {
-		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
-		dataSource.setUsername("postgres");
-		dataSource.setPassword("isdb62");
-		dataSource.setInitialSize(5); // Initial connections in the pool
-		dataSource.setMaxTotal(20); // Maximum number of active connections
-		dataSource.setMaxIdle(10); // Maximum idle connections
-		dataSource.setMinIdle(5); // Minimum idle connections
-		dataSource.setMaxWait(Duration.ofMillis(10000));
-		return dataSource;
-	}
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+//      dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/DemoDB");
+        dataSource.setUsername("postgres");
+//		dataSource.setPassword("isdb62");
+        dataSource.setPassword("1245");
+        dataSource.setInitialSize(5); // Initial connections in the pool
+        dataSource.setMaxTotal(20); // Maximum number of active connections
+        dataSource.setMaxIdle(10); // Maximum idle connections
+        dataSource.setMinIdle(5); // Minimum idle connections
+        dataSource.setMaxWait(Duration.ofMillis(10000));
+        return dataSource;
+    }
 
-	@Bean
-	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-		return new JdbcTemplate(dataSource);
-	}
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
 
-	@Bean
-	public DataSourceTransactionManager transactionManager(DataSource dataSource) {
-		return new DataSourceTransactionManager(dataSource);
-	}
+    @Bean
+    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
 }
